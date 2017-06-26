e7525f9328dcf47cf66ffca99dbd43c9a60b3d32
Neota Moe <neota.moe@gmail.com>
2017-06-26 08:22:24 -0500

    Initial Commit

---
+++ BNRSwiftChapter15MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BNRSwiftChapter15MonsterTown
+//
+//  Created by Neota Moe on 6/26/17.
+//  Copyright © 2017 Neota Moe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
